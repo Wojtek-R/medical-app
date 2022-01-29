186f514b663edc9f05499bc8ea462715d3d96ab9
Wojtek-R <46377805+Wojtek-R@users.noreply.github.com>
2022-01-29 23:57:53 +0000

    finishing login welcome page + creating routing for glossary.

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -12,17 +12,17 @@
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laravel.com/docs">Glossary</a></div>
+            <svg fill="none" stroke="purple" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+            <div class="ml-4 text-lg leading-7 font-semibold">Glossary</div>
         </div>
 
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
+            <div class="mt-2 text-sm">
                 Look up and read more about your medical terms.
             </div>
         </div>
 
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-12 mt-6 w-48">
+        <button onclick="window.location='{{ url("/glossary") }}'" style="background-color: #8732a8" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-12 mt-6 w-48">
             Go to Glossary
         </button>
 
@@ -30,48 +30,55 @@
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laracasts.com">Play and learn</a></div>
+            <svg fill="purple" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 280 280" class="w-8 h-8 text-gray-400">  >
+	<path d="M183.028,43.028H96.972C43.501,43.028,0,86.529,0,140s43.501,96.972,96.972,96.972h86.057
+		C236.499,236.972,280,193.471,280,140S236.499,43.028,183.028,43.028z M183.028,218.972H96.972C53.427,218.972,18,183.545,18,140
+		s35.427-78.972,78.972-78.972h86.057C226.573,61.028,262,96.455,262,140S226.573,218.972,183.028,218.972z"/>
+                <path d="M177.029,129.326l-42.208-35.29c-9.058-7.573-22.838-1.133-22.838,10.674v70.58c0,11.807,13.78,18.248,22.838,10.674
+		l42.208-35.29C183.681,145.113,183.681,134.887,177.029,129.326z"/>
+    </svg>
+            <div class="ml-4 text-lg leading-7 font-semibold">Play and learn</div>
         </div>
 
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
+            <div class="mt-2 text-sm">
                 Play games with medical terms to expand your knowledge.
             </div>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
+        <button style="background-color: #8732a8" class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
             Play games
         </button>
     </div>
 
     <div class="p-6 border-t border-gray-200">
         <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://tailwindcss.com/">My progress - Coming Soon</a></div>
+            <svg fill="purple" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400">><defs><style>.cls-1{fill:none;}</style></defs><title>development</title><path d="M8,4V8H4V4H8M2,2v8h8V2Z"/><path d="M18,7v4H14V7h4M12,5v8h8V5Z"/><path d="M8,16v4H4V16H8M2,14v8h8V14Z"/><path d="M22,10v6H16v6H10v8H30V10Zm-4,8h4v4H18ZM16,28H12V24h4Zm6,0H18V24h4Zm6,0H24V24h4Zm0-6H24V18h4Zm-4-6V12h4v4Z"/><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/></svg>
+            <div class="ml-4 text-lg leading-7 font-semibold">My progress - Coming Soon</div>
         </div>
 
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
+            <div class="mt-2 text-sm">
                 This feature is currently under development.
             </div>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
+        <button style="background-color: #8732a8" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
             Button
         </button>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Notes - Coming Soon</div>
+
+            <svg fill="purple" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400"> ><defs><style>.cls-1{fill:none;}</style></defs><title>development</title><path d="M8,4V8H4V4H8M2,2v8h8V2Z"/><path d="M18,7v4H14V7h4M12,5v8h8V5Z"/><path d="M8,16v4H4V16H8M2,14v8h8V14Z"/><path d="M22,10v6H16v6H10v8H30V10Zm-4,8h4v4H18ZM16,28H12V24h4Zm6,0H18V24h4Zm6,0H24V24h4Zm0-6H24V18h4Zm-4-6V12h4v4Z"/><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/></svg>
+            <div class="ml-4 text-lg leading-7 font-semibold">Notes - Coming Soon</div>
         </div>
 
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
+            <div class="mt-2 text-sm">
                 This feature is currently under development.
             </div>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
+        <button style="background-color: #8732a8" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded rounded ml-12 mt-6 w-48">
             Button
         </button>
 
